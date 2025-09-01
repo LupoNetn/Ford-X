4376d2948614fac2013810e54e65f6bb72be1727
oluwatobi <luponet07@gmail.com>
2025-09-01 06:18:41 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
+
+import HomePage from './pages/HomePage'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Services from './pages/Services'
+import NotFound from './pages/NotFound'
+
+const App = () => {
+  return (
+    <>
+     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/services' element={<Services />}/>
+        <Route path='*' element={<NotFound />}/>
+      </Routes>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
