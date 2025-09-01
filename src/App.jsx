058f94fc428768f112066d2b52e8d4a9bf2c24fd
oluwatobi <luponet07@gmail.com>
2025-09-01 06:25:57 -0400

    finished project setup, file,theme and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route} from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HomePage from './pages/HomePage'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Services from './pages/Services'
-import NotFound from './pages/NotFound'
+//Routes Imports
+import Layout from "./PagesLayout/Layout";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <>
-     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage />}/>
-        <Route path='/about' element={<About />}/>
-        <Route path='/contact' element={<Contact />}/>
-        <Route path='/services' element={<Services />}/>
-        <Route path='*' element={<NotFound />}/>
-      </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
