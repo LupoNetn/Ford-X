9b8a054198b5fb025c66b8e53e64f5b3a2b7359e
oluwatobi <luponet07@gmail.com>
2025-09-01 14:19:51 -0400

    homepage hero section setup

--- src/PagesLayout/Navbar.jsx
+++ src/PagesLayout/Navbar.jsx
@@ -1,11 +1,98 @@
-import React from 'react'
+import { ArrowBigRight, Menu, X } from "lucide-react";
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const navLinks = [
+  { name: "Home", link: "/" },
+  { name: "About", link: "/about" },
+  { name: "Services", link: "/services" },
+  { name: "Contact", link: "/services" },
+];
 
 const Navbar = () => {
-  return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+  const [open, setOpen] = useState(false);
 
-export default Navbar
+  return (
+    <>
+      <nav className="px-custom py-5">
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="text-4xl font-bold">
+              Ford
+              <span className="text-transparent bg-gradient-to-r from-red-500 to-blue-950 bg-clip-text">
+                X
+              </span>
+            </h1>
+          </div>
+          <div className="hidden sm:flex gap-10 items-center">
+            <ul className="flex sm:gap-5 lg:gap-10 items-center">
+              {navLinks.map((item, idx) => (
+                <button>
+                  <Link key={idx} to={item.link}>
+                    <li className="text-blue-950 md:text-sm lg:text-lg hover:text-blue-700">
+                      {item.name}
+                    </li>
+                  </Link>
+                </button>
+              ))}
+            </ul>
+            <button className="flex gap-3 items-center bg-blue-950 text-amber-50 px-4 py-2 rounded-[40px] md:text-sm lg:text-lg">
+              Get Started{" "}
+              <span className="bg-white text-blue-950 p-1 rounded-full">
+                <ArrowBigRight />
+              </span>
+            </button>
+          </div>
+
+          {/* Mobile Navigation */}
+
+          <div className="block sm:hidden">
+            {open ? (
+              <div className="relative">
+                <button
+                  onClick={() => setOpen((prev) => !prev)}
+                  className="cursor-pointer text-blue-950"
+                >
+                  <X size={30} />
+                </button>
+
+                {/* Links */}
+                <div className="bg-gradient from-blue-100 to-gray-500 shadow-md rounded-lg p-10 absolute right-7">
+                  <div className="flex flex-col md:hidden gap-10 items-left">
+                    <ul className="flex flex-col gap-10 items-left">
+                      {navLinks.map((item, idx) => (
+                        <button onClick={() => setOpen((prev) => !prev)}>
+                          <Link key={idx} to={item.link}>
+                            <li className="text-blue-950 text-lg hover:text-blue-700">
+                              {item.name}
+                            </li>
+                          </Link>
+                        </button>
+                      ))}
+                    </ul>
+                    <button className="flex gap-3 items-center bg-blue-950 text-amber-50 px-10 py-1 rounded-[40px] text-md">
+                      Get Started{" "}
+                      <span className="bg-white text-blue-950 p-1 rounded-full">
+                        <ArrowBigRight />
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <button
+                onClick={() => setOpen((prev) => !prev)}
+                className="cursor-pointer text-blue-950"
+              >
+                <Menu size={30} />
+              </button>
+            )}
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,106 @@
-import React from 'react'
+import React from "react";
+import { ArrowBigRight, Expand, LocationEditIcon, Play } from "lucide-react";
 
 const HomePage = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <>
+      <div className="px-4 mt-7">
+        {/* Mobile Headline */}
+        <div className="md:hidden text-blue-950 text-left flex flex-col gap-5 my-10">
+          <h1 className="text-5xl md:text-4xl lg:text-7xl">
+            Delivering More Than Just Freight
+          </h1>
+          <p className="text-lg md:text-lg">
+            We prioritize reliability, precision, and strong partnerships in
+            every shipment ensuring a seamless and efficient logistics
+            experience from start to finish.
+          </p>
 
-export default HomePage
+          <button className="flex gap-3 items-center bg-blue-950 text-amber-50 px-4 py-2 rounded-[40px] w-[50%]">
+            Get Started{" "}
+            <span className="bg-white text-blue-950 p-1 rounded-full">
+              <ArrowBigRight />
+            </span>
+          </button>
+
+          <button className="flex gap-3 items-center bg-white text-blue-950 border border-blue-950 px-4 py-2 rounded-[40px] w-[50%]">
+            Learn More{" "}
+            <span className="bg-blue-950 text-white p-1 rounded-full">
+              <ArrowBigRight />
+            </span>
+          </button>
+        </div>
+
+        <div className="px-custom bg-[url('/hero-port1.jpg')] rounded-md w-full mx-auto max-md:h-[500px] py-10 bg-cover relative overflow-hidden">
+          {/* BG Image details */}
+          <div className="absolute top-10 right-4 flex items-center gap-2">
+            <span className="bg-white text-blue-950 rounded-full p-2">
+              <LocationEditIcon size={16} />
+            </span>
+            <div className="bg-white text-blue-950 rounded-3xl p-2">
+              <p className="text-xs">Port of Singapore</p>
+            </div>
+          </div>
+
+          <div className="absolute bottom-10 right-4 w-[200px] md:w-[200px] md:p-3 p-1 flex items-center gap-3 bg-white text-blue-950 rounded-md md:rounded-2xl">
+            <div>
+              <p className="max-sm:text-xs">
+                We <span className="text-red-500">cover</span> shipment around
+                the <span className="text-red-500">globe</span>
+              </p>
+              <div className="flex gap-2 mt-2">
+                <span className="bg-blue-950 text-white rounded-full p-1">
+                  <Play size={16} />
+                </span>
+                <span className="bg-white text-blue-950 rounded-1 p-1">
+                  <Expand size={16} />
+                </span>
+              </div>
+            </div>
+            <div>
+              <img
+                src="/plane-in-port.jpeg"
+                alt="shipment"
+                className="h-full"
+              />
+            </div>
+          </div>
+
+          {/* Overlay */}
+          <div className="absolute inset-0 bg-white/20 bg-opacity-[2px] rounded-md pointer-events-none"></div>
+
+          <div className="relative z-10">
+            <div className="hidden text-white text-left w-[90%] md:w-[70%] lg:w-[50%] md:flex flex-col gap-5 mt-20 md:mt-32 lg:mt-40">
+              <h1 className="text-6xl md:text-4xl lg:text-7xl">
+                Delivering More Than Just Freight
+              </h1>
+              <p className="text-lg md:text-lg">
+                We prioritize reliability, precision, and strong partnerships in
+                every shipment ensuring a seamless and efficient logistics
+                experience from start to finish.
+              </p>
+
+              <div className="flex items-center gap-4">
+                <button className="flex gap-3 items-center bg-blue-950 text-amber-50 px-4 py-2 rounded-[40px]">
+                  Get Started{" "}
+                  <span className="bg-white text-blue-950 p-1 rounded-full">
+                    <ArrowBigRight />
+                  </span>
+                </button>
+
+                <button className="flex gap-3 items-center bg-white text-blue-950 border border-blue-950 px-4 py-2 rounded-[40px]">
+                  Learn More{" "}
+                  <span className="bg-blue-950 text-white p-1 rounded-full">
+                    <ArrowBigRight />
+                  </span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
